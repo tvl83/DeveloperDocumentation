67d6970411dfc682ff5411867aeaa787bbced8a6
Corey Earwood <corey@orbotix.com>
2015-01-06 14:53:04 -0700

    Get language switching working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var port = process.env.PORT || 3000;
 var app = express();
 var jf = require('jsonfile');
 
+app.disable('etag');
+
 var objectiveCiOSJSON;
 
 jf.readFile('src/content/getting-started/objectivec.json', function(err, obj) {
@@ -17,17 +19,33 @@ app.set('view engine', 'ejs');
 
 app.use('/', function(req, res, next) {
 	session = req.session;
+	console.log(session.language);
 	queryParserForSession(req);
 	next();
 });
 
 app.get('/getting-started', function (req, res) {
-	var pageString = "getting-started-objc";
+	var pageString = "getting-started-connect-objc";
 
-	if (session.language == "swift") {
-		pageString = "getting-started-swift";
+	//Prevent browser caching causing language switching to break
+	res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+
+	if (session.language != undefined) {
+		console.log("Serving page for language " + session.language);
+		pageString = "getting-started-connect-" + session.language;
 	}
-	res.sendfile('build/getting-started/getting-started-connect-objc/index.html');
+
+	if (req.query.language != undefined) {
+		console.log(req.path);
+		res.writeHead(301,
+			{Location: 'http://'+req.headers.host+req.path}
+		);
+		res.end();
+	} else {
+		res.sendfile('build/getting-started/' + pageString + '/index.html');
+	}
 });
 
 app.get('/session_test', function(req, res) {
@@ -43,8 +61,23 @@ app.get('/session_test', function(req, res) {
 
 function queryParserForSession(request) {
 	if (request.query.language != undefined) {
-		session.language = request.query.language;
-	};
+		if (isLanguageValid(request) == true) {
+			session.language = request.query.language;
+			console.log("Setting language to " + session.language);
+		}
+	}
+
+	console.log(request.query.language + " " + session.language);
+}
+
+function isLanguageValid(request) {
+	if (request.query.language == undefined) {return true}
+
+	if (request.query.language == 'objc' || request.query.language == 'swift' || request.query.language == 'android') {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 app.use(express.static(__dirname + '/build'));
