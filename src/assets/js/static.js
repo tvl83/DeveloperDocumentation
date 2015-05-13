9a7fa3051aa558ec35a84ad00273ef3fcba24783
Enrique Canals <enriquecanals@gmail.com>
2015-05-13 14:53:21 -0400

    Disable access to Ollie docs

--- src/assets/js/static.js
+++ src/assets/js/static.js
@@ -3,7 +3,8 @@
     $(".sphero.oval1, .sphero.oval2, .sphero.oval3").toggleClass("active");
   });
 
-  $(".ollie-selector").hover(function() {
-    $(".ollie.oval1, .ollie.oval2, .ollie.oval3").toggleClass("active");
-  });
+  // Disable Ollie docs for now...
+  // $(".ollie-selector").hover(function() {
+  //   $(".ollie.oval1, .ollie.oval2, .ollie.oval3").toggleClass("active");
+  // });
 })(jQuery);
